06be8be220111de78aa7a42cd91659304189f9d2
jasmithaarepalli-ops <jasmithaarepalli@gmail.com>
2026-02-07 10:19:32 +0530

    Add files via upload

---
+++ Module 1/cprogram1a.c
@@ -0,0 +1,8 @@
+// C Program for output statement (printf) 
+#include<stdio.h> 
+int main() 
+{ 
+int a=10; 
+printf("Value of a=%d",a); 
+return 0; 
+} 
---
+++ Module 1/cprogram6.c
@@ -0,0 +1,13 @@
+//Number Comparison (find biggest number) 
+#include <stdio.h> 
+int main() 
+{ 
+    int a, b; 
+    printf("Enter two numbers: "); 
+    scanf("%d %d", &a, &b); 
+    if(a > b) 
+        printf("%d is greater\n", a); 
+    else 
+        printf("%d is greater\n", b); 
+    return 0; 
+}
