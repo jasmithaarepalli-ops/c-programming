66343a972624ed844c17cb5468401ff692475f29
jasmithaarepalli-ops <jasmithaarepalli@gmail.com>
2026-02-07 10:20:35 +0530

    Delete Module 3 directory

--- Module 3/cprogram41.c
+++
@@ -1,6 +0,0 @@
-//Extract username from email
-#include <stdio.h>
-int main(){ char e[100]; int i;
-gets(e);
-for(i=0;e[i]!='@';i++) printf("%c",e[i]);
-}
--- Module 3/cprogram45.c
+++
@@ -1,8 +0,0 @@
-//Read a string and print it
-#include <stdio.h>
-int main() {
-    char s[100];
-    fgets(s, 100, stdin);
-    printf("%s", s);
-    return 0;
-}
